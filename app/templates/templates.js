40a1b225896684b2ef52141b4064d527da82c818
Sean Esteva <seangeleno@gmail.com>
2016-01-16 09:36:51 -0800

    fixed angular display bug, was missing s in the templates.js templateStore.templates

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -1,8 +1,11 @@
-angular.module('templateStore.template',['ngRoute'])
+angular.module('templateStore.templates',['ngRoute'])
   .config(['$routeProvider', function($routeProvider){
     $routeProvider.
       when('/templates', {
         templateUrl: 'templates/templates.html',
         controller: 'TemplatesCtrl'
-      })
+      });
+  }])
+  .controller('TemplatesCtrl', ['$scope',function($scope){
+    console.log($scope);
   }])
